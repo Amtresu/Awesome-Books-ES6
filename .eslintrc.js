47b882bbbce318591df779548718e4e20aa6f06a
Amtresu <conor.follette@gmail.com>
2022-08-08 20:48:58 +0900

    restoring linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,15 @@
-{
-  "env": {
-      "browser": true,
-      "es6": true,
-      "jest": true
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
   },
-  "parser": "babel-eslint",
-  "parserOptions": {
-      "ecmaVersion": 2018,
-      "sourceType": "module"
-  },
-  "extends": [
-      "airbnb-base"
+  extends: [
+    'standard'
   ],
-  "rules": {
-      "no-shadow": "off",
-      "no-param-reassign": "off",
-      "eol-last": "off",
-      "import/extensions": [
-          1,
-          {
-              "js": "always",
-              "json": "always"
-          }
-      ]
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  "ignorePatterns": [
-      "dist/",
-      "build/"
-  ]
+  rules: {
+  }
 }
